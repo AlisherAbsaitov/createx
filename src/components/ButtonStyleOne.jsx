6bd5e03a660bb8369b4a5204c66ac7fb3a216e10
SobirjonovInomjon <huvikitos@gmail.com>
2025-12-25 19:22:41 +0500

    Sobirjonov secont section

--- src/components/ButtonStyleOne.jsx
+++ src/components/ButtonStyleOne.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ButtonStyleOne(props) {
   return (
     <React.Fragment>
-        <button className='bg-[linear-gradient(45deg,#FF4038,#E85808)] py-2 px-3 rounded-md border-2 border-[#ff4038] text-white hover:text-[#ff4038] duration-300 hover:bg-[linear-gradient(0deg,transparent,transparent)]'>{props.text}</button>
+        <button className='bg-[linear-gradient(45deg,#FF4038,#E85808)] py-2 px-7 rounded-md border-2 w-fit border-[#ff4038] text-white hover:text-[#ff4038] duration-300 hover:bg-[linear-gradient(0deg,transparent,transparent)]'>{props.text}</button>
     </React.Fragment>
   )
 }
